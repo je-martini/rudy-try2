9e9a8bbf0d085c859f132b9f609025b5abaf6fb0
je-martini <jesusmartini2017@gmail.com>
2022-09-17 22:00:49 -0400

    adding the togge to the movile menu

--- js/header.js
+++ js/header.js
@@ -1,1 +1,9 @@
+// to show the movile_menu
 const button_center_section_nav_header = document.querySelector('.button_center_section_nav_header');
+const a_container_button = document.querySelector('.a_container_button');
+
+button_center_section_nav_header.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu () {
+    a_container_button.classList.toggle('inactive');
+}
